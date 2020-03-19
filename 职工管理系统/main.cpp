0873171234783f35992a81463824f7d1c4e7f219
jeff tian <tianwenbo6666@163.com>
2020-03-19 22:39:19 +0800

    完成菜单界面面和输入保护

---
+++ 职工管理系统/main.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include"workerManager.h"
+using namespace std;
+
+int main()
+{
+	WorkerManager w;
+	int select = 0;
+	while (true)
+	{
+		w.Show_Menu();//展示菜单
+		cout << "请输入选择" << endl;
+		cin >> select;
+		if (cin.fail()) //若流操作失败，则返回true
+		{
+			cout << "请输入整数" << endl;
+			cin.clear();
+			cin.ignore(1024, '\n');
+			system("pause");
+			system("cls");
+		}
+		else
+		{
+			switch (select)
+			{
+			case 0: //退出系统
+				break;
+			case 1: //添加职工
+				break;
+			case 2: //显示职工
+				break;
+			case 3: //删除职工
+				break;
+			case 4: //修改职工
+				break;
+			case 5: //查找职工
+				break;
+			case 6: //排序职工
+				break;
+			case 7: //清空文件
+				break;
+			default:
+				cout << "请输入0-7" << endl;
+				system("pause");
+				system("cls");
+				break;
+			}
+		}
+		
+		
+	}
+
+
+
+
+	system("pause");
+	return 0;
+}
